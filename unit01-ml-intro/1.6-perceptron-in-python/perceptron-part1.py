0a3ca4ed873769e6d177fd58e820a823982e22e8
henrikh.baghramyan@gmail.com <35291491+chester1986@users.noreply.github.com>
2024-03-17 09:51:32 +0000

    1st unit done

--- unit01-ml-intro/1.6-perceptron-in-python/perceptron-part1.py
+++ unit01-ml-intro/1.6-perceptron-in-python/perceptron-part1.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 df = pd.read_csv(
-    "unit01-ml-intro/1.6-perceptron-in-python/perceptron_toydata-truncated.txt",
+    r"unit01-ml-intro\1.6-perceptron-in-python\perceptron_toydata-truncated.txt",
     sep="\t",
 )
 
@@ -11,10 +12,12 @@ y_train = df["label"].values
 
 
 class Perceptron:
-    def __init__(self, num_features):
+    def __init__(self, num_features, alpha):
+        np.random.seed(123)
         self.num_features = num_features
-        self.weights = [0.0 for _ in range(num_features)]
-        self.bias = 0.0
+        self.weights = [np.random.uniform(-0.5, 0.5) for _ in range(num_features)]
+        self.bias = np.random.uniform(-0.5, 0.5)
+        self.alpha = alpha
 
     def forward(self, x):
         weighted_sum_z = self.bias
@@ -33,12 +36,14 @@ class Perceptron:
         error = true_y - prediction
 
         # update
+        error *= self.alpha
         self.bias += error
         for i, _ in enumerate(self.weights):
             self.weights[i] += error * x[i]
 
         return error
-    
+
+
 def train(model, all_x, all_y, epochs):
 
     for epoch in range(epochs):
@@ -51,6 +56,10 @@ def train(model, all_x, all_y, epochs):
 
         print(f"Epoch {epoch+1} errors {error_count}")
 
+        if not error_count:
+            break
+
+
 def compute_accuracy(model, all_x, all_y):
 
     correct = 0.0
@@ -60,6 +69,7 @@ def compute_accuracy(model, all_x, all_y):
         correct += int(prediction == y)
 
     return correct / len(all_y)
+
 
 def plot_boundary(model):
 
@@ -74,10 +84,12 @@ def plot_boundary(model):
 
     return x1_min, x1_max, x2_min, x2_max
 
-ppn = Perceptron(num_features=2)
+
+ppn = Perceptron(num_features=2, alpha=1.0)
 ppn.update([-1.2, 2.4], 1)
 
-train(model=ppn, all_x=X_train, all_y=y_train, epochs=5)
+train(model=ppn, all_x=X_train, all_y=y_train, epochs=10)
+print(f"The weights are {ppn.weights}")
 train_acc = compute_accuracy(ppn, X_train, y_train)
 
 x1_min, x1_max, x2_min, x2_max = plot_boundary(ppn)
